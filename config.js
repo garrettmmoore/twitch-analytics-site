e2809ca2f239b8961874a8fc8c0f73fb14ce47db
Garrett Moore <garrettmarkmoore@gmail.com>
2020-06-13 04:44:12 -0700

    Add config to manage credentials

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  client_id: process.env.TWITCH_CLIENT_ID,
+  client_secret: process.env.TWITCH_CLIENT_SECRET,
+  grant_type: process.env.TWITCH_GRANT_TYPES,
+  scopes: process.env.TWITCH_SCOPES,
+  twitch_oauth_url: process.env.TWITCH_OAUTH_API_URL,
+  twitch_api_url: process.env.TWITCH_API_URL
+};
+
+module.exports = config;
