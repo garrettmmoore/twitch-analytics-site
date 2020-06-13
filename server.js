6765037320a7f1c9288198f7112cb99b32313030
Garrett Moore <garrettmarkmoore@gmail.com>
2020-06-13 04:44:49 -0700

    Add express server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config({ path: `${__dirname}/.env` });
+
+// Enable middleware for logging in development mode
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+app.use('/users', require('./routes/users'));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${port}`
+  );
+});
