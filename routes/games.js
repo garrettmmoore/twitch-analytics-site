b4be4b961fc512dc9d370344dcb4e5a9ceb0927f
Garrett Moore <garrettmarkmoore@gmail.com>
2020-06-13 18:08:41 -0700

    Fix to display top games
    The /top route must be first in order to be hit before the /:id.

--- routes/games.js
+++ routes/games.js
@@ -7,6 +7,44 @@ const router = express.Router();
 const { client_id, client_secret, twitch_api_url } = config;
 
 /**
+ * Gets games sorted by number of current viewers on Twitch, most popular first.
+ */
+
+router.get('/top', async (req, res) => {
+  if (!client_id || !client_secret) {
+    return res.status(401).json({
+      message: 'You must provide a Client ID and a Client Secret.'
+    });
+  }
+
+  // Get access token
+  const { access_token } = await tools.getTwitchAccessToken();
+
+  try {
+    // Set headers
+    const headers = {
+      'Client-ID': client_id,
+      Authorization: `Bearer ${access_token}`
+    };
+
+    // Get data from Twitch API
+    const response = await fetch(`${twitch_api_url}/games/top`, {
+      headers
+    });
+
+    // Format data and send to the client
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: 'Server Error.'
+    });
+  }
+});
+
+/**
  * Gets game information by game ID or name.
  */
 router.get('/:id', async (req, res) => {
@@ -31,44 +69,6 @@ router.get('/:id', async (req, res) => {
 
     // Get data from Twitch API
     const response = await fetch(`${twitch_api_url}/games?${id}`, {
-      headers
-    });
-
-    // Format data and send to the client
-    const data = await response.json();
-    res.json(data);
-  } catch (err) {
-    console.error(err);
-
-    res.status(500).json({
-      message: 'Server Error.'
-    });
-  }
-});
-
-/**
- * Gets games sorted by number of current viewers on Twitch, most popular first.
- */
-
-router.get('/top', async (req, res) => {
-  if (!client_id || !client_secret) {
-    return res.status(401).json({
-      message: 'You must provide a Client ID and a Client Secret.'
-    });
-  }
-
-  // Get access token
-  const { access_token } = await tools.getTwitchAccessToken();
-
-  try {
-    // Set headers
-    const headers = {
-      'Client-ID': client_id,
-      Authorization: `Bearer ${access_token}`
-    };
-
-    // Get data from Twitch API
-    const response = await fetch(`${twitch_api_url}/games/top`, {
       headers
     });
 
