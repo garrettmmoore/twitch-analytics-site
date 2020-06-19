3b8588062f75e0a700ef610eed7880dc196eec9a
Garrett Moore <garrettmarkmoore@gmail.com>
2020-06-18 23:59:26 -0700

    Update server to use cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 // Load environment variables
 dotenv.config({ path: `${__dirname}/.env` });
 
 const app = express();
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -18,13 +20,13 @@ app.use('/users', require('./routes/users'));
 app.use('/games', require('./routes/games'));
 
 // Handle production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(__dirname + '/public/'));
+// if (process.env.NODE_ENV === 'production') {
+// Set static folder
+app.use(express.static(__dirname + '/public/'));
 
-  // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
+// Handle SPA
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+// }
 
 const port = process.env.PORT || 8000;
 
