3bac1cf352179370149151999afd9842add1aee3
Garrett Moore <garrettmarkmoore@gmail.com>
2020-06-25 23:42:42 -0700

    Update express server to run on cloud functions

--- functions/index.js
+++ functions/index.js
@@ -28,10 +28,12 @@ if (process.env.NODE_ENV === 'production') {
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-const port = process.env.PORT || 8000;
+// const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${port}`
-  );
-});
+// app.listen(port, () => {
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${port}`
+//   );
+// });
+
+module.exports = { app };
