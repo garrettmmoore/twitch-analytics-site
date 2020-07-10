14f8620ea73d9fe46603f378cd0e9a520f3bd431
Garrett Moore <garrettmarkmoore@gmail.com>
2020-07-09 23:35:26 -0700

    Refactor to use ProfileData component

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import Extensions from './Extensions';
+import ProfileData from './ProfileData/ProfileData';
 
 const Results = ({ data }) => {
   let toggle = data.panel;
-  let results = Object.entries(data) || [];
 
   return (
-    <>
-      {!toggle ? (
-        <ul className="twitch-data-list">
-          {results.map(item => (
-            <li key={item[0]}>
-              {item[0] === 'profile_image_url' ? (
-                <img src={item[1]} alt="new" />
-              ) : (
-                <span value={item[1]}>
-                  {item[0]}: {item[1]}
-                </span>
-              )}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <Extensions data={data} />
-      )}
-    </>
+    <>{!toggle ? <ProfileData data={data} /> : <Extensions data={data} />}</>
   );
 };
 
